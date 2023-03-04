355f21ea8a4fd6b53248680f23190465b0918f8e
RAFAEL GALDINO DE ALMEIDA <96850401+al-meida@users.noreply.github.com>
2023-03-04 05:10:47 -0300

    Seção 5
    Exercícios do 1 ao 5 da Seção 5

---
+++ exerciciosJava/src/secao5/Exercicio1.java
@@ -0,0 +1,23 @@
+package secao5;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a;
+		a = sc.nextInt();
+		
+		if (a < 0) {
+			System.out.println("NEGATIVO");
+		}
+		else {
+			System.out.println("NÃO NEGATIVO");
+		}
+		
+		sc.close();
+	}
+
+}
