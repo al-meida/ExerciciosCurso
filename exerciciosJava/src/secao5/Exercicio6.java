73b92ef2fe87e25a63860c4890e6f7b95162ad2e
RAFAEL GALDINO DE ALMEIDA <96850401+al-meida@users.noreply.github.com>
2023-03-05 04:41:36 -0300

    Exercicios da Seção 5
    Upload os Exercícios 6-8 da Seção 5.

---
+++ exerciciosJava/src/secao5/Exercicio6.java
@@ -0,0 +1,30 @@
+package secao5;
+
+import java.util.Scanner;
+
+public class Exercicio6 {
+
+	public static void main(String[] args) {
+		// Exercicio uri1037
+
+		Scanner sc = new Scanner(System.in);
+
+		double x = sc.nextDouble();
+
+		if (x > 0 && x < 25.01) {
+			System.out.println("Intervalo (0,25)");
+		} else if (x > 25 && x < 50.01) {
+			System.out.println("Intervalo (25,50)");
+
+		} else if (x > 50 && x < 75.01) {
+			System.out.println("Intervalo (50, 75)");
+		} else if (x > 75 && x < 100.01) {
+			System.out.println("Intervalo (75, 100)");
+		} else {
+			System.out.println("Fora do intervalo");
+		}
+
+		sc.close();
+	}
+
+}
---
+++ exerciciosJava/src/secao5/Exercicio7.java
@@ -0,0 +1,35 @@
+package secao5;
+
+import java.util.Scanner;
+
+public class Exercicio7 {
+
+	public static void main(String[] args) {
+		//Exercicio uri1041
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double x = sc.nextDouble();
+		double y = sc.nextDouble();
+		
+		
+		if (x == 0 && y == 0) {
+			System.out.println("Origem");
+		} else if (x > 0 && y > 0 ) {
+			System.out.println("Q1");
+		} else if (x < 0 && y > 0 ) {
+			System.out.println("Q2");
+		} else if (x < 0 && y < 0) {
+			System.out.println("Q3");
+		} else if (x > 0 && y < 0) {
+			System.out.println("Q4");
+		} else if (x == 0){
+			System.out.println("Eixo X");
+		} else {
+			System.out.println("Eixo Y");
+		}
+		
+		sc.close();
+	}
+
+}
