9d6e46b2423174d127420f889c382ed2fbd748d4
RAFAEL GALDINO DE ALMEIDA <96850401+al-meida@users.noreply.github.com>
2023-03-22 05:08:55 -0300

    Exercício 2
    Remoção de println e implementando \n para pular linhas na classe Program.

--- exerciciosJava/src/secao8/aula69/Exercicio2/application/Program.java
+++ exerciciosJava/src/secao8/aula69/Exercicio2/application/Program.java
@@ -7,30 +7,27 @@ import secao8.aula69.Exercicio2.entities.Employee;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		Employee employee = new Employee();
-		
-		System.out.println("Name: ");
+
+		System.out.print("Name: ");
 		employee.name = sc.nextLine();
-		System.out.println("Salário Bruto: ");
+		System.out.print("Salário Bruto: ");
 		employee.grossSalary = sc.nextDouble();
-		System.out.println("Imposto: ");
+		System.out.print("Imposto: ");
 		employee.tax = sc.nextDouble();
-		
-		System.out.printf("%s, $ %.2f%n", employee.name, employee.NetSalary());
-	
-		System.out.println("\nWhich percentage to increase salary?");
+
+		System.out.printf("\n%s, $ %.2f%n", employee.name, employee.NetSalary());
+
+		System.out.print("\nWhich percentage to increase salary? ");
 		double percentage = sc.nextDouble();
 		employee.IncreaseSalary(percentage);
-		
-		System.out.printf("%s, $ %.2f%n", employee.name, employee.grossSalary);
-		
-		
-		
-		
+
+		System.out.printf("\n%s, $ %.2f%n", employee.name, employee.grossSalary);
+
 		sc.close();
 	}
 
